32e92a2bb75179ed8839fa09889116b0e347d6b4
Sonu <notifications3972@gmail.com>
2017-10-04 09:53:45 -0700

    Added all other files

--- app/src/main/java/gllc/tech/blocksteps/MainActivity.java
+++ app/src/main/java/gllc/tech/blocksteps/MainActivity.java
@@ -1,13 +1,166 @@
 package gllc.tech.blocksteps;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.crashlytics.android.Crashlytics;
+import com.google.firebase.FirebaseApp;
+
+import gllc.tech.blocksteps.Services.SendStepsService;
+import gllc.tech.blocksteps.Services.StepService;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    String uniqueID;
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setup();
+
+        Intent i = new Intent(this, StepService.class);
+        startService(i);
+
+        Fragment stepFragment = new StepFragment();
+
+        //getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, stepFragment).commit();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, stepFragment,"StepFragment").commit();
+
     }
+
+    public void setup() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Fabric.with(this, new Crashlytics());
+        FirebaseApp.initializeApp(this);
+
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        editor = sharedPref.edit();
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Reloading From Blockchain", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                StepFragment stepFragment = (StepFragment) getSupportFragmentManager().findFragmentByTag("StepFragment");
+
+                for (int i=0; i>=-6; i--) {stepFragment.loadBlockchainData(i);}
+            }
+        });
+
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Sending Steps to Blockchain", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                Intent i = new Intent(MainActivity.this, SendStepsService.class);
+                startService(i);
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        uniqueID = getHardwareId(this);
+        editor.putString("uniqueId",uniqueID).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public static String getHardwareId(Context context) {
+        return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+
 }
